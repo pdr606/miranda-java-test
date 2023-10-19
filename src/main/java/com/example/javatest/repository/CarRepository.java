2892e466597efa639a4216f23a4f8f358cf662cf
Pedro Guarnieri <119369768+pdr606@users.noreply.github.com>
2023-10-19 09:19:02 -0300

    Create car repository

---
+++ src/main/java/com/example/javatest/repository/CarRepository.java
@@ -0,0 +1,7 @@
+package com.example.javatest.repository;
+
+import com.example.javatest.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
