9690b2b87a0f8d6d72a641ee8b42c9701c8237c8
Pedro Guarnieri <119369768+pdr606@users.noreply.github.com>
2023-10-20 15:42:42 -0300

    Remove ValidationExcenption of GlobalExceptionHandler

--- src/main/java/com/example/javatest/exceptions/global/GlobalExceptionHandler.java
+++ src/main/java/com/example/javatest/exceptions/global/GlobalExceptionHandler.java
@@ -4,7 +4,9 @@ package com.example.javatest.exceptions.global;
 import com.example.javatest.exceptions.CarDuplicateException;
 import com.example.javatest.exceptions.CarNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.json.JsonParseException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,8 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.format.DateTimeParseException;
+import java.util.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -33,15 +35,5 @@ public class GlobalExceptionHandler {
         HttpStatus status = HttpStatus.NOT_FOUND;
         return new StandardError(Instant.now(), status.value(), error, ex.getMessage(), request.getRequestURI());
     }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex){
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) ->{
-            String errorMessage = error.getDefaultMessage();
-            errors.put("ValidationError", errorMessage);
-        });
-        return errors;
-    }
+    
 }
