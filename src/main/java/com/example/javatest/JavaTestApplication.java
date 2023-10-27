5d61f19dd9002df99b6965f099a40ac9b4dfd7f0
Pedro Guarnieri <pedroguarnieric@gmail.com>
2023-10-27 19:08:27 -0300

    Update JavaTestApplication.test

--- src/main/java/com/example/javatest/JavaTestApplication.java
+++ src/main/java/com/example/javatest/JavaTestApplication.java
@@ -3,7 +3,7 @@ package com.example.javatest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
 
 @EnableCaching
 @SpringBootApplication
